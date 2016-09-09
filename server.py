e334bef8580fb05964a137ec9b74ac54dcc9c87d
arturosaco <arturo.sanchezcorrea@gmail.com>
2016-09-09 16:46:15 +0100

    update

--- server.py
+++ server.py
@@ -8,5 +8,5 @@ class HelloWorld(object):
 
 
 if __name__ == '__main__':
-    cherrypy.config.update({'server.socket_port': 443})
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 443})
     cherrypy.quickstart(HelloWorld())

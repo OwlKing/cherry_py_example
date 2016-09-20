a3ced34568ccdc059a0aa3358eb0492f698e3b9e
arturosaco <arturo.sanchezcorrea@gmail.com>
2016-09-20 10:56:04 +0100

    minor fix

--- aux_clone_repo.py
+++ aux_clone_repo.py
@@ -1,5 +1,5 @@
 import os
 from subprocess import call
 
-repo_string = "https://" + os.getenv("GITHUB_USER") + ":" + os.getenv("GITHUB_PASS") + "@github.com/indexds/companies_tracker.git"
+repo_string = "https://" + os.environ["GITHUB_USER"] + ":" + os.environ["GITHUB_PASS"] + "@github.com/indexds/companies_tracker.git"
 call(["git", "clone", repo_string])

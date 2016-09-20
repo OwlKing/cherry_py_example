1f8f157f75ce9b3716b8a7386d4442d8f3961095
arturosaco <arturo.sanchezcorrea@gmail.com>
2016-09-20 18:01:16 +0100

    nothing

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 import cherrypy
 import os
-from subprocess import call
 
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        hello = os.getenv("GITHUB_USER")
-        return hello
+        return "hello world"
 
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 80})
@@ -14,3 +12,5 @@ if __name__ == '__main__':
 
 
 # docker -H localhost:2374 service create --replicas 2 --name cherrypy --publish 443:443 arturosaco/cherrypy_example python server.py
+
+ # docker -H localhost:2374 run --name cherrypy -d -p 8080:8080 lawouach/cherrypy-hello-world

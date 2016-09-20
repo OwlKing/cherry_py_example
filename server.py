443db115962b65642de8532bac0cbd2319625a3d
arturosaco <arturo.sanchezcorrea@gmail.com>
2016-09-20 14:07:55 +0100

    changed port

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import cherrypy
 import os
-
+from subprocess import call
 
 class HelloWorld(object):
     @cherrypy.expose
@@ -8,7 +8,9 @@ class HelloWorld(object):
         hello = os.getenv("GITHUB_USER")
         return hello
 
-
 if __name__ == '__main__':
-    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 443})
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 80})
     cherrypy.quickstart(HelloWorld())
+
+
+# docker -H localhost:2374 service create --replicas 2 --name cherrypy --publish 443:443 arturosaco/cherrypy_example python server.py

7a5732c114a73933b82e64b3cc59d6bc70eb391a
arturosaco <arturo.sanchezcorrea@gmail.com>
2016-11-08 15:43:57 +0000

    Added printing of env variable

--- server.py
+++ server.py
@@ -4,13 +4,9 @@ import os
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return "hello world"
+        return "hello world! " + os.getenv("GITHUB_USER")
 
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 80})
     cherrypy.quickstart(HelloWorld())
 
-
-# docker -H localhost:2374 service create --replicas 2 --name cherrypy --publish 443:443 arturosaco/cherrypy_example python server.py
-
- # docker -H localhost:2374 run --name cherrypy -d -p 8080:8080 lawouach/cherrypy-hello-world

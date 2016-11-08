448ec3ae15310423fec0c7e9bb632b1b82bd7adc
arturosaco <arturo.sanchezcorrea@gmail.com>
2016-11-08 17:59:09 +0000

    b

--- aux_clone_repo.py
+++ aux_clone_repo.py
@@ -1,5 +1,6 @@
 import os
-from subprocess import call
+from git import Repo
 
-repo_string = "https://" + os.environ["GITHUB_USER"] + ":" + os.environ["GITHUB_PASS"] + "@github.com/indexds/companies_tracker.git"
-call(["git", "clone", repo_string])
+# repo_string = "https://" + os.getenv("GITHUB_USER") + ":" + os.getenv("GITHUB_PASS") + "@github.com/indexds/companies_tracker.git"
+repo_string = "https://github.com/" + os.getenv("GITHUB_USER") +"/cherry_py_example.git"
+Repo.clone_from(repo_string, "cherry_py_example")

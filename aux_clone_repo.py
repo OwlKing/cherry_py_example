0cebc577b8faec55d76992df8eafafbfe5cbf008
arturosaco <arturo.sanchezcorrea@gmail.com>
2016-11-09 11:13:10 +0000

    :/

--- aux_clone_repo.py
+++ aux_clone_repo.py
@@ -1,6 +1,6 @@
 import os
 from git import Repo
 
-repo_string = "https://" + os.getenv("GITHUB_USER") + ":" + os.getenv("GITHUB_PASS") + "@github.com/indexds/companies_tracker.git"
+repo_string = "https://" + os.getenv("GITHUB_USER") + ":" + os.getenv("GITHUB_PASS") + "@github.com/indexds/alexa_cherrypy.git"
 # repo_string = "https://github.com/" + os.getenv("GITHUB_USER") +"/cherry_py_example.git"
-Repo.clone_from(repo_string, "companies_tracker")
+Repo.clone_from(repo_string, "cherrypy_server")
